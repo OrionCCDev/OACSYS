fb730355661fe9befaab02ed46d5ec93341687a8
OrionCCDev <developer@orioncc.com>
2025-01-31 10:52:34 +0400

    add two shortcuts

--- resources/views/employees/receives.blade.php
+++ resources/views/employees/receives.blade.php
@@ -11,7 +11,19 @@
                 <h5 class="hk-sec-title">Employee Receives</h5>
                 <h2>Received Receives</h2>
             </section>
-
+            <section class="hk-sec-wrapper">
+                <h5 class="hk-sec-title">Make New Receive</h5>
+                <a href="{{ route('receive.create') }}" class="btn btn-gradient-primary btn-wth-icon btn-rounded icon-right">
+                    <span class="btn-text">Make New</span>
+                    <span class="icon-label">
+                        <span class="feather-icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right-circle">
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <polyline points="12 16 16 12 12 8"></polyline>
+                                <line x1="8" y1="12" x2="16" y2="12"></line>
+                            </svg></span> </span>
+                </a>
+            </section>
             <div class="hk-row">
                 <div class="col-sm-12">
                     {{-- start of content --}}
