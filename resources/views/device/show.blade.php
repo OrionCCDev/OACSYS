3554ba68e95cb1b6bb899194d62652cabb5d0b48
OrionCCDev <a.sayed@orioncc.com>
2025-02-02 00:49:22 -0700

    force

--- resources/views/device/show.blade.php
+++ resources/views/device/show.blade.php
@@ -76,6 +76,7 @@
                                             <span>Type: </span></span>
                                             <span class="ml-5 text-dark">{{ $device->device_type ?? 'No Device Type' }} </span>
                                     </li>
+
                                     <li class="list-group-item" style="font-size: 35px">
                                         <span>
                                             <span>Price: </span></span>
