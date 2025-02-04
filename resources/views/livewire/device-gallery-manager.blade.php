3ae7beb0819ef21995087ab149cbcbaf3b8c17a9
OrionCCDev <developer@orioncc.com>
2025-02-04 12:36:33 +0400

    sdfsgs

--- resources/views/livewire/device-gallery-manager.blade.php
+++ resources/views/livewire/device-gallery-manager.blade.php
@@ -1,10 +1,10 @@
 <div>
     <div class="row">
-        @foreach($device->media as $image)
+        @foreach($device->getMedia('Device_image') as $img )
             <div class="col-md-3 mb-4">
                 <div class="position-relative">
-                    <img src="{{ asset($image->getUrl()) }}" class="img-fluid rounded" alt="Device image">
-                    <button wire:click="deleteImage({{ $image->id }})"
+                    <img src="{{ asset('media/' . $img->id . '/' . $img->file_name) }}" class="img-fluid rounded" alt="Device image">
+                    <button wire:click="deleteImage({{ $img->id }})"
                             class="btn btn-danger btn-sm position-absolute"
                             style="top: 10px; right: 10px;">
                         <i class="zmdi zmdi-delete"></i>
