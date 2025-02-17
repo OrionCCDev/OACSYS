c7f165cc95b1528a9300cac250c5a6bf9eef838f
OrionCCDev <developer@orioncc.com>
2025-02-17 10:33:43 +0400

    a

--- app/Models/ClientEmployee.php
+++ app/Models/ClientEmployee.php
@@ -12,7 +12,7 @@ class ClientEmployee extends Model implements HasMedia
     protected $guarded = [];
 
     public function client(){
-        return $this->belongsTo(client::class , 'client_id' , 'id');
+        return $this->belongsTo(Client::class , 'client_id' , 'id');
     }
     public function clearance(){
         return $this->hasMany(Clearance::class , 'client_employee_id' , 'id');
