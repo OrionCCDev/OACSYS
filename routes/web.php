85e1a19a26067aea2ffe7f32cc501e4683a77abe
OrionCCDev <developer@orioncc.com>
2025-02-18 20:28:59 +0400

    update sims

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,8 @@ Route::get('/update-employees', function () {
     return view('employees.uploadEmployeesUpdated');
 })->middleware(['auth', 'role:o-super-admin|o-admin']);
 
+Route::get('/update-sims', [SimCardController::class , 'create'])->middleware(['auth', 'role:o-super-admin|o-admin']);
+
 
 Route::post('/update-employees', [EmployeeController::class, 'updateEmployees'])
     ->middleware(['auth', 'role:o-super-admin|o-admin'])->name('employees.updateFromExcel');
