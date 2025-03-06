1f709d16f0328a16085e67e8575a12cf76b9ef12
OrionCCDev <developer@orioncc.com>
2025-03-06 09:14:38 +0400

    update ahmed abdelkader

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -9,63 +9,75 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserSeeder extends Seeder
 {
+
+
     /**
      * Run the database seeds.
      */
-    protected $initialUsers = [
-        [
-            'name' => 'AhmedSayed',
-            'email' => 'a.sayed@it.orion',
-            'password' => 'THEgh0$t',
-            'image' => 'sayed.jpg',
-            'orion_role_lvl' => 'o-super-admin',
-            'employee_id' => 3358
-        ],
-        [
-            'name' => 'AhmedFaisal',
-            'email' => 'a.faisal@it.orion',
-            'password' => 'AHMED@ALHAG@128',
-            'image' => 'faisal.jpg',
-            'orion_role_lvl' => 'o-super-admin',
-            'employee_id' => 3170
-        ],
-        [
-            'name' => 'hr',
-            'email' => 'hr@orioncc.com',
-            'password' => 'hr@123',
-            'image' => 'hr.jpg',
-            'orion_role_lvl' => 'o-hr',
-            'employee_id' => 614
-        ],
-    ];
+    // protected $initialUsers = [
+    //     [
+    //         'name' => 'AhmedSayed',
+    //         'email' => 'a.sayed@it.orion',
+    //         'password' => 'THEgh0$t',
+    //         'image' => 'sayed.jpg',
+    //         'orion_role_lvl' => 'o-super-admin',
+    //         'employee_id' => 3358
+    //     ],
+    //     [
+    //         'name' => 'AhmedFaisal',
+    //         'email' => 'a.faisal@it.orion',
+    //         'password' => 'AHMED@ALHAG@128',
+    //         'image' => 'faisal.jpg',
+    //         'orion_role_lvl' => 'o-super-admin',
+    //         'employee_id' => 3170
+    //     ],
+    //     [
+    //         'name' => 'hr',
+    //         'email' => 'hr@orioncc.com',
+    //         'password' => 'hr@123',
+    //         'image' => 'hr.jpg',
+    //         'orion_role_lvl' => 'o-hr',
+    //         'employee_id' => 614
+    //     ],
+    // ];
     public function run(): void
-    {
-        $managers = Employee::where('type', 'manager')->get();
-        foreach ($managers as $manager) {
-            $user = User::create([
-                'name' => $manager->name,
-                'email' => $manager->orion_email ?? 'manager@orion.com',
-                'password' => bcrypt('manager@' . $manager->id),
-                'image' => $manager->profile_image,
-                'orion_role_lvl' => 'o-manager',
-                'employee_profile_id' => $manager->id
-            ]);
-            $user->addRole($user['orion_role_lvl']);
+     {
+        $manager = Employee::where('id', 132)->first();
+        $user = User::create([
+            'name' => 'AHMED ABDELKADER	',
+            'email' => 'ahmed@orioncc.com',
+            'password' => bcrypt('manager@132'),
+            'image' => $manager->profile_image,
+            'orion_role_lvl' => 'o-manager',
+            'employee_profile_id' => $manager->id
+        ]);
+        $user->addRole($user['orion_role_lvl']);
+    //     $managers = Employee::where('type', 'manager')->get();
+    //     foreach ($managers as $manager) {
+    //         $user = User::create([
+    //             'name' => $manager->name,
+    //             'email' => $manager->orion_email ?? 'manager@orion.com',
+    //             'password' => bcrypt('manager@' . $manager->id),
+    //             'image' => $manager->profile_image,
+    //             'orion_role_lvl' => 'o-manager',
+    //             'employee_profile_id' => $manager->id
+    //         ]);
+    //         $user->addRole($user['orion_role_lvl']);
 
-        }
-            foreach ($this->initialUsers as $userData) {
-            $user =  User::updateOrCreate(
-                ['email' => $userData['email']], // Find by email
-                [
-                    'name' => $userData['name'],
-                    'password' => bcrypt($userData['password']),
-                    'image' => $userData['image'],
-                    'orion_role_lvl' => $userData['orion_role_lvl'],
-                    'employee_profile_id' => Employee::where('employee_id','=',$userData['employee_id'])->value('id') ?? null
-                ]
-            );
-            $user->addRole($userData['orion_role_lvl']);
-        }
+    //     }
+    //         foreach ($this->initialUsers as $userData) {
+    //         $user =  User::updateOrCreate(
+    //             ['email' => $userData['email']], // Find by email
+    //             [
+    //                 'name' => $userData['name'],
+    //                 'password' => bcrypt($userData['password']),
+    //                 'image' => $userData['image'],
+    //                 'orion_role_lvl' => $userData['orion_role_lvl'],
+    //                 'employee_profile_id' => Employee::where('employee_id','=',$userData['employee_id'])->value('id') ?? null
+    //             ]
+    //         );
+    //         $user->addRole($userData['orion_role_lvl']);
+    //     }
 
         // // Uncomment to automatically create users for all managers
         // $this->createManagerUsers();
@@ -112,5 +124,5 @@ class UserSeeder extends Seeder
 
         //      $user->addRole('o-manager');
         //  }
-    }
+     }
 }
