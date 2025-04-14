d017d24ae6bd80020396002ea66adfb0e17f68e2
ahmdsydysf <a.sayed.xc@gmail.com>
2025-04-14 23:18:37 +0400

    finish

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -31,7 +31,10 @@ class Device extends Model implements HasMedia
     {
         return $this->belongsToMany(Clearance::class , 'device_and_sim_clearances' , 'device_id' ,'clearance_id' , 'id' , 'id');
     }
-
+    public function deviceAndSimReceives()
+    {
+        return $this->hasMany(DeviceAndSimReceive::class, 'device_id', 'id');
+    }
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('Device_image');
