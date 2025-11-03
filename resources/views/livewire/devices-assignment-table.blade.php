35d1a9b2ca572d4d17dc749456ea8857875b48da
ahmdsydysf <a.sayed.xc@gmail.com>
2025-11-03 09:52:17 +0400

    Fix checkbox alignment in assets transfer table - align checkboxes to top of cell for better visibility with multi-line names

--- resources/views/livewire/devices-assignment-table.blade.php
+++ resources/views/livewire/devices-assignment-table.blade.php
@@ -15,7 +15,7 @@
                     <thead>
                         <tr>
                             <th style="width: 50px; text-align: center; vertical-align: middle;">
-                                <input type="checkbox" id="selectAll">
+                                <input type="checkbox" id="selectAll" style="width: 18px; height: 18px; cursor: pointer;">
                             </th>
                             <th>img</th>
                             <th>Code</th>
@@ -27,17 +27,17 @@
                     <tbody>
                         @foreach($devices as $device)
                         <tr>
-                            <td style="text-align: center; vertical-align: middle;">
+                            <td style="text-align: center; vertical-align: top; padding-top: 15px;">
                                 <input type="checkbox"
                                     wire:model.live="selectedDevices"
                                     value="{{ $device->id }}"
-                                    style="width: 18px; height: 18px; cursor: pointer;">
+                                    style="width: 20px; height: 20px; cursor: pointer; margin: 0;">
                             </td>
-                            <td style="vertical-align: middle;"> <img src="{{ asset('X-Files/Dash/imgs/devices/' . $device->main_image) }}" alt="" srcset="" width="75" height="75"></td>
-                            <td style="vertical-align: middle;">{{ $device->device_code }}</td>
-                            <td style="vertical-align: middle;">{{ $device->device_name }}</td>
-                            <td style="vertical-align: middle;">{{ $device->device_type }}</td>
-                            {{-- <td style="vertical-align: middle;">{{ $device->status }}</td> --}}
+                            <td style="vertical-align: top; padding-top: 10px;"> <img src="{{ asset('X-Files/Dash/imgs/devices/' . $device->main_image) }}" alt="" srcset="" width="75" height="75"></td>
+                            <td style="vertical-align: top; padding-top: 15px;">{{ $device->device_code }}</td>
+                            <td style="vertical-align: top; padding-top: 15px;">{{ $device->device_name }}</td>
+                            <td style="vertical-align: top; padding-top: 15px;">{{ $device->device_type }}</td>
+                            {{-- <td style="vertical-align: top; padding-top: 15px;">{{ $device->status }}</td> --}}
                         </tr>
                         @endforeach
                     </tbody>
@@ -61,3 +61,24 @@
         </div>
     </div>
 </div>
+
+<style>
+    /* Ensure checkboxes align with first line of text */
+    .table tbody tr td {
+        vertical-align: top !important;
+    }
+    
+    /* Align checkbox to top of cell */
+    .table tbody tr td:first-child {
+        padding-top: 15px !important;
+    }
+    
+    /* Ensure checkboxes are properly sized and visible */
+    .table input[type="checkbox"] {
+        width: 20px !important;
+        height: 20px !important;
+        cursor: pointer;
+        margin: 0;
+        vertical-align: top;
+    }
+</style>
