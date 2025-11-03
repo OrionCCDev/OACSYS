868993f39a1c77483308c19dd35beb21e84a7b58
ahmdsydysf <a.sayed.xc@gmail.com>
2025-11-03 10:00:05 +0400

    Fix detached checkbox and scroll issues - removed Select All checkbox and refined CSS to prevent layout breaks

--- resources/views/livewire/devices-assignment-table.blade.php
+++ resources/views/livewire/devices-assignment-table.blade.php
@@ -14,9 +14,7 @@
                 <table class="table table-hover">
                     <thead>
                         <tr>
-                            <th style="width: 50px; text-align: center; vertical-align: middle;">
-                                <input type="checkbox" id="selectAll" style="width: 18px; height: 18px; cursor: pointer;">
-                            </th>
+                            <th style="width: 50px; text-align: center;">Select</th>
                             <th>img</th>
                             <th>Code</th>
                             <th>Name</th>
@@ -27,17 +25,19 @@
                     <tbody>
                         @foreach($devices as $device)
                         <tr>
-                            <td style="text-align: center; vertical-align: top; padding-top: 15px;">
+                            <td class="text-center align-top" style="padding-top: 12px;">
                                 <input type="checkbox"
                                     wire:model.live="selectedDevices"
                                     value="{{ $device->id }}"
-                                    style="width: 20px; height: 20px; cursor: pointer; margin: 0;">
+                                    class="device-checkbox">
                             </td>
-                            <td style="vertical-align: top; padding-top: 10px;"> <img src="{{ asset('X-Files/Dash/imgs/devices/' . $device->main_image) }}" alt="" srcset="" width="75" height="75"></td>
-                            <td style="vertical-align: top; padding-top: 15px;">{{ $device->device_code }}</td>
-                            <td style="vertical-align: top; padding-top: 15px;">{{ $device->device_name }}</td>
-                            <td style="vertical-align: top; padding-top: 15px;">{{ $device->device_type }}</td>
-                            {{-- <td style="vertical-align: top; padding-top: 15px;">{{ $device->status }}</td> --}}
+                            <td class="align-top" style="padding-top: 8px;">
+                                <img src="{{ asset('X-Files/Dash/imgs/devices/' . $device->main_image) }}" alt="" width="75" height="75">
+                            </td>
+                            <td class="align-top" style="padding-top: 12px;">{{ $device->device_code }}</td>
+                            <td class="align-top" style="padding-top: 12px;">{{ $device->device_name }}</td>
+                            <td class="align-top" style="padding-top: 12px;">{{ $device->device_type }}</td>
+                            {{-- <td class="align-top" style="padding-top: 12px;">{{ $device->status }}</td> --}}
                         </tr>
                         @endforeach
                     </tbody>
@@ -63,22 +63,17 @@
 </div>
 
 <style>
-    /* Ensure checkboxes align with first line of text */
-    .table tbody tr td {
-        vertical-align: top !important;
-    }
-    
-    /* Align checkbox to top of cell */
-    .table tbody tr td:first-child {
-        padding-top: 15px !important;
-    }
-    
     /* Ensure checkboxes are properly sized and visible */
-    .table input[type="checkbox"] {
-        width: 20px !important;
-        height: 20px !important;
+    .device-checkbox {
+        width: 20px;
+        height: 20px;
         cursor: pointer;
         margin: 0;
-        vertical-align: top;
+    }
+    
+    /* Prevent any layout issues with table responsiveness */
+    .table-responsive {
+        overflow-x: auto;
+        -webkit-overflow-scrolling: touch;
     }
 </style>
