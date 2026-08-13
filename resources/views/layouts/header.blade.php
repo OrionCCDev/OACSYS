2fe35577234395be1e9f34c7a712b0140704af2a
Ah-Sy-Yf <59507720+ahmdsydysf@users.noreply.github.com>
2026-08-13 15:09:57 +0400

    Add missing sidebar link to Department Assets page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -323,6 +323,9 @@
                                         <li class="nav-item {{ Request::is('project-assets*') ? 'active' : '' }}" style="margin-bottom: 7px">
                                             <a class="nav-link" style="font-size: 20px" href="{{ route('project-assets.index') }}"><span class="crystal-nav-icon crystal-nav-icon-sm"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4a2 2 0 0 0 1-1.73V8z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg></span>Project Assets</a>
                                         </li>
+                                        <li class="nav-item {{ Request::is('department-assets*') ? 'active' : '' }}" style="margin-bottom: 7px">
+                                            <a class="nav-link" style="font-size: 20px" href="{{ route('department-assets.index') }}"><span class="crystal-nav-icon crystal-nav-icon-sm"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg></span>Department Assets</a>
+                                        </li>
 
                                         {{-- @elseif (Auth::user()->hasRole('o-hr') ) --}}
 
